4b5d08abdab7bba978586a7edebae52d781a4a50
Laura Brandt <lbrandt@reverbnation.com>
2020-01-14 12:18:33 -0500

    Add custom buttons to Snackbar

--- dist/cjs/chunk-3dffe6e7.js
+++ dist/cjs/chunk-3dffe6e7.js
@@ -1,54 +1,54 @@
-'use strict';
+'use strict'
 
 var SlotComponent = {
-  name: 'BSlotComponent',
-  props: {
-    component: {
-      type: Object,
-      required: true
-    },
-    name: {
-      type: String,
-      default: 'default'
-    },
-    scoped: {
-      type: Boolean
-    },
+    name: 'BSlotComponent',
     props: {
-      type: Object
+        component: {
+            type: Object,
+            required: true
+        },
+        name: {
+            type: String,
+            default: 'default'
+        },
+        scoped: {
+            type: Boolean
+        },
+        props: {
+            type: Object
+        },
+        tag: {
+            type: String,
+            default: 'div'
+        },
+        event: {
+            type: String,
+            default: 'hook:updated'
+        }
     },
-    tag: {
-      type: String,
-      default: 'div'
+    methods: {
+        refresh: function refresh() {
+            this.$forceUpdate()
+        },
+        isVueComponent: function isVueComponent() {
+            return this.component && this.component._isVue
+        }
     },
-    event: {
-      type: String,
-      default: 'hook:updated'
-    }
-  },
-  methods: {
-    refresh: function refresh() {
-      this.$forceUpdate();
+    created: function created() {
+        if (this.isVueComponent()) {
+            this.component.$on(this.event, this.refresh)
+        }
     },
-    isVueComponent: function isVueComponent() {
-      return this.component && this.component._isVue;
+    beforeDestroy: function beforeDestroy() {
+        if (this.isVueComponent()) {
+            this.component.$off(this.event, this.refresh)
+        }
+    },
+    render: function render(createElement) {
+        if (this.isVueComponent()) {
+            return createElement(this.tag, {}, this.scoped ? this.component.$scopedSlots[this.name](this.props) : this.component.$slots[this.name])
+        }
     }
-  },
-  created: function created() {
-    if (this.isVueComponent()) {
-      this.component.$on(this.event, this.refresh);
-    }
-  },
-  beforeDestroy: function beforeDestroy() {
-    if (this.isVueComponent()) {
-      this.component.$off(this.event, this.refresh);
-    }
-  },
-  render: function render(createElement) {
-    if (this.isVueComponent()) {
-      return createElement(this.tag, {}, this.scoped ? this.component.$scopedSlots[this.name](this.props) : this.component.$slots[this.name]);
-    }
-  }
-};
+}
 
-exports.SlotComponent = SlotComponent;
+exports.SlotComponent = SlotComponent
