777e487279b469b9d5220c92013f0836ee8852cc
Laura Brandt <lbrandt@reverbnation.com>
2020-02-05 13:58:21 -0500

    allow readonly attribute in datepicker on mobile

--- dist/esm/icon.js
+++ dist/esm/icon.js
@@ -1,15 +1,15 @@
-import './chunk-f2006744.js';
-import './helpers.js';
-import './chunk-b76a6c1d.js';
-import { I as Icon } from './chunk-c8434a6f.js';
-export { I as BIcon } from './chunk-c8434a6f.js';
-import { r as registerComponent, u as use } from './chunk-cca88db8.js';
+import './chunk-f2006744.js'
+import './helpers.js'
+import './chunk-b76a6c1d.js'
+import { I as Icon } from './chunk-c8434a6f.js'
+import { r as registerComponent, u as use } from './chunk-cca88db8.js'
+export { I as BIcon } from './chunk-c8434a6f.js'
 
 var Plugin = {
-  install: function install(Vue) {
-    registerComponent(Vue, Icon);
-  }
-};
-use(Plugin);
+    install: function install(Vue) {
+        registerComponent(Vue, Icon)
+    }
+}
+use(Plugin)
 
-export default Plugin;
+export default Plugin
