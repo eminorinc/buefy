46ce22c05932c646020ec435178906a720ea724e
Laura Brandt <lbrandt@reverbnation.com>
2020-02-06 16:27:54 -0500

    Merge reverb

--- src/components/snackbar/Snackbar.vue
+++ src/components/snackbar/Snackbar.vue
@@ -11,9 +11,27 @@
             <div
                 v-if="actionText"
                 class="action"
-                @click="action"
+                @click="action(onAction)"
                 :class="type">
                 <button class="button">{{ actionText }}</button>
+            </div>
+            <div
+                v-if="buttons && buttons.length > 0"
+                style="margin-left: 1em">
+                <button
+                    v-for="(button, i) in buttons"
+                    :key="i"
+                    :class="button.classes"
+                    class="button is-small"
+                    style="margin-right: 1em"
+                    @click="action(button.action)">
+                    <span v-if="button.icon" class="icon">
+                        <b-icon
+                            :icon="button.icon"
+                            size="is-small"/>
+                    </span>
+                    <span>{{ button.text }}</span>
+                </button>
             </div>
         </div>
     </transition>
@@ -38,6 +56,10 @@ export default {
         indefinite: {
             type: Boolean,
             default: false
+        },
+        buttons: {
+            type: Array,
+            default: () => []
         }
     },
     data() {
@@ -50,10 +72,16 @@ export default {
         * Click listener.
         * Call action prop before closing (from Mixin).
         */
-        action() {
-            this.onAction()
+        action(cb) {
+            cb()
             this.close()
         }
     }
 }
 </script>
+
+<style scoped>
+.button.is-text:hover, .button.is-text:focus {
+    background-color: initial;
+}
+</style>
