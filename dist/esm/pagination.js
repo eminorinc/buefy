439e2457d1eef8a32d85793a353ab66ce655f86f
Laura Brandt <lbrandt@reverbnation.com>
2020-02-14 11:53:29 -0500

    branch combining changes from v0.8.9-2 and v0.8.9-3

--- dist/esm/pagination.js
+++ dist/esm/pagination.js
@@ -1,17 +1,17 @@
-import './chunk-f2006744.js'
-import './helpers.js'
-import './chunk-b76a6c1d.js'
-import './chunk-c8434a6f.js'
-import { r as registerComponent, u as use } from './chunk-cca88db8.js'
-import { P as Pagination, a as PaginationButton } from './chunk-4dc010ac.js'
-export { P as BPagination, a as BPaginationButton } from './chunk-4dc010ac.js'
+import './chunk-f2006744.js';
+import './helpers.js';
+import './chunk-b76a6c1d.js';
+import './chunk-c8434a6f.js';
+import { r as registerComponent, u as use } from './chunk-cca88db8.js';
+import { P as Pagination, a as PaginationButton } from './chunk-4dc010ac.js';
+export { P as BPagination, a as BPaginationButton } from './chunk-4dc010ac.js';
 
 var Plugin = {
-    install: function install(Vue) {
-        registerComponent(Vue, Pagination)
-        registerComponent(Vue, PaginationButton)
-    }
-}
-use(Plugin)
+  install: function install(Vue) {
+    registerComponent(Vue, Pagination);
+    registerComponent(Vue, PaginationButton);
+  }
+};
+use(Plugin);
 
-export default Plugin
+export default Plugin;
--- dist/esm/timepicker.js
+++ dist/esm/timepicker.js
@@ -1,23 +1,23 @@
-import './chunk-f2006744.js'
-import './helpers.js'
-import './chunk-b76a6c1d.js'
-import './chunk-03b1476b.js'
-import './chunk-c8434a6f.js'
-import { r as registerComponent, u as use } from './chunk-cca88db8.js'
-import './chunk-70383fcd.js'
-import './chunk-636acb08.js'
-import './chunk-b3471279.js'
-import './chunk-b98d635b.js'
-import './chunk-169e5faa.js'
-import './chunk-9c209565.js'
-import { T as Timepicker } from './chunk-fed5b1a3.js'
-export { T as BTimepicker } from './chunk-fed5b1a3.js'
+import './chunk-f2006744.js';
+import './helpers.js';
+import './chunk-b76a6c1d.js';
+import './chunk-03b1476b.js';
+import './chunk-c8434a6f.js';
+import { r as registerComponent, u as use } from './chunk-cca88db8.js';
+import './chunk-70383fcd.js';
+import './chunk-636acb08.js';
+import './chunk-b3471279.js';
+import './chunk-b98d635b.js';
+import './chunk-169e5faa.js';
+import './chunk-9c209565.js';
+import { T as Timepicker } from './chunk-fed5b1a3.js';
+export { T as BTimepicker } from './chunk-fed5b1a3.js';
 
 var Plugin = {
-    install: function install(Vue) {
-        registerComponent(Vue, Timepicker)
-    }
-}
-use(Plugin)
+  install: function install(Vue) {
+    registerComponent(Vue, Timepicker);
+  }
+};
+use(Plugin);
 
-export default Plugin
+export default Plugin;
